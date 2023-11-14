c55d43c6b7cc80ddffc9e15bc77c7eb6ead42e0b
Ramzzz067 <147711716+Ramzzz067@users.noreply.github.com>
2023-11-14 19:30:34 +0100

    app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import requests
+import json
 
 response = requests.get('https://punapi.rest/api/pun')
 if response:
-    print(response.text)
+    data = json.loads(response.text)
+    print(data["pun"])
 else:
     print('An error has occurred.')
